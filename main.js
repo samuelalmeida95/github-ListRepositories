76ba778d47855e8568dee5a958f584f8ea3d1b2e
samuelalmeida95 <samuelalmeida95>
2021-02-27 18:05:40 -0300

    melhorando o css do projeto

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ let user = document.getElementById("user")
 let conteudo = document.querySelector("ul")
 
 adicionar = () => {
-  conteudo.innerHTML = ''
-  
-  axios.get(`https://api.github.com/users/${user.value}/repos`)
+  conteudo.innerHTML = "";
 
-    .then((response) => {   
-        response.data
-        .map((repositorioTitulo) => {
-                conteudo.innerHTML += `<li>${repositorioTitulo.name}</li>`
-            })  
-            console.log(response)    
+  axios
+    .get(`https://api.github.com/users/${user.value}/repos`)
+
+    .then((response) => {
+      response.data.map((repositorioTitulo) => {
+        conteudo.innerHTML += `<li>${repositorioTitulo.name}</li>`;
+      });
+      console.log(response);
     })
 
     .catch((error) => {
-        alert(` ${error.message}`)
-    })
-}
+      alert(` ${error.message}`);
+    });    
+};
+
 
